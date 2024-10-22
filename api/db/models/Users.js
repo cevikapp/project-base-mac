bc85814c524f27a9279b17050ddbde270c47fc5d
Mustafa <cevikapp@gmail.comq:qq:backgit config --global user.name mustafa>
2024-10-22 14:46:19 +0300

    #7 user auth yapısı kuruldu

--- api/db/models/Users.js
+++ api/db/models/Users.js
@@ -1,4 +1,10 @@
 const mongoose = require("mongoose");
+const {PASS_LENGTH, HTTP_CODES} = require("../../config/Enum");
+const is = require("is_js");
+const CustomError = require("../../lib/Error");
+const bcrypt = require("bcryptjs");
+
+
 const schema = mongoose.Schema({
     email: {type: String, required: true, unique:true },
     password: {type: String, required:true },
@@ -14,7 +20,16 @@ const schema = mongoose.Schema({
 });
 
 class Users extends mongoose.Model {
-
+    
+    validPassword(password){
+        return bcrypt.compareSync(password, this.password);
+    }
+    
+    static validateFieldsBeforeAuth(email, password){
+        if(typeof password !== "string" || password.length < PASS_LENGTH || is.not.email(email) ){
+            throw new CustomError(HTTP_CODES.UNAUTHORIZED, "Validation Error", "email or password wrong");
+        }
+    }
 }
 
 schema.loadClass(Users);
